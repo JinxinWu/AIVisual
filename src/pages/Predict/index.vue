744f4a4311d03a84bf4d9eb6df7d9663a322c79f
JinxinWu <1762563628@qq.com>
2023-11-18 14:32:37 +0800

    应用界面更改

--- src/pages/Predict/index.vue
+++ src/pages/Predict/index.vue
@@ -10,11 +10,11 @@
         type="flex"
         class="row-bg"
         justify="center"
-        v-for="(o, index) in models.length/3+1"
+        v-for="(o, index) in models.length / 3 + 1"
       >
         <el-col
           :span="5"
-          v-for="(model, index) in models.slice(index*3, index*3+3)"
+          v-for="(model, index) in models.slice(index * 3, index * 3 + 3)"
           :key="index"
           :offset="index > 0 ? 1 : 0"
         >
@@ -27,12 +27,34 @@
               <span style="font-size: 18px">{{ model.name }}</span>
               <div class="bottom clearfix">
                 <time class="time">{{ currentDate }}</time>
-                <el-button type="text" class="button">操作按钮</el-button>
+                <el-button
+                  type="text"
+                  class="button"
+                  @click="dialogVisible = true"
+                  >立即体验</el-button
+                >
               </div>
             </div>
           </el-card>
         </el-col>
       </el-row>
+      <el-dialog title="应用体验" :visible.sync="dialogVisible">
+        <div style="font-size: 18px; margin-bottom: 10px;"><p>请上传测试集</p></div>
+        <el-upload
+          class="upload-demo"
+          drag
+          :http-request="uploadFile"
+          multiple
+        >
+          <i class="el-icon-upload"></i>
+          <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+          <div class="el-upload__tip" slot="tip">
+            只能上传csv文件，且不超过500kb
+          </div>
+        </el-upload>
+        <div v-show="resultShow" style="font-size: 18px; margin-top: 20px; margin-bottom: 10px;"><p>请下载结果文件</p></div>
+        <a v-show="resultShow" :href="resultUrl"><el-button type="primary" icon="el-icon-download">结果文件</el-button></a>
+      </el-dialog>
     </el-main>
   </el-container>
 </template>
@@ -47,6 +69,9 @@ export default {
   },
   data() {
     return {
+      dialogVisible: false,
+      resultShow: false,
+      resultUrl: "",
       models: [
         {
           name: "AI绘画",
@@ -75,7 +100,58 @@ export default {
         new Date().getDate(),
     };
   },
-  methods: {},
+  methods: {
+    uploadFile(item) {
+      console.log("文件上传中........");
+      const suffix = item.file.name.slice(
+        ((item.file.name.lastIndexOf(".") - 1) >>> 0) + 1
+      );
+      //文件类型转化
+      const type = 0;
+      if (suffix == ".csv") {
+        type = 1;
+      } else if (suffix == ".xlsx") {
+        type = 2;
+      } else if (suffix == ".db") {
+        type = 3;
+      } else {
+        type = 4;
+      }
+      console.log(type);
+
+      if (!this.isValidFile(item.file)) {
+        this.$message.warning(`文件格式不符合要求！`);
+      } else {
+        //上传文件的需要formdata类型;所以要转
+        let FormDatas = new FormData();
+        FormDatas.append("file", item.file);
+        axios({
+          method: "post",
+          url: "/guo/test/upload?user_id=" + this.user_id,
+          headers: this.headers,
+          timeout: 30000,
+          data: FormDatas,
+        }).then((res) => {
+          const result = res.data;
+          if (result.msg.includes("文件上传成功")) {
+            this.$message.success("文件上传成功");
+            //将后端传来的数据存储
+            this.trainId = result.trainId;
+            this.data_url = result.url;
+            axios({
+              method: "get",
+              url: `/guo/test/upload?url=${this.data_url}&type=${type}`,
+              headers: this.headers,
+            }).then((res) => {
+               const reply = res.data.reply;
+            });
+          } else {
+            this.$message.warning(`文件上传失败，请重新上传`);
+          }
+        });
+      }
+    },
+  },
   watch: {},
 };
 </script>
@@ -83,7 +159,8 @@ export default {
 <style lang="less" scoped>
 .title {
   margin-top: 20px;
-  font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
+  font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB",
+    "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
   font-size: 2em;
   text-align: center;
 }
