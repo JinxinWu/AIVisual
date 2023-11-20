d5d96dc6f764fe5d2f39b5037747f34f2dd487d4
JinxinWu <1762563628@qq.com>
2023-11-20 17:10:15 +0800

    logo上传

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,6 @@
 <script>
 import Header from '@/components/Header/index.vue'
 
-
-
 export default {
   name: 'App',
   components: {
@@ -17,39 +15,5 @@ export default {
 </script>
 
 <style lang="less" scoped>
-// Container
-.el-header,
-.el-footer {
-  background-color: #B3C0D1;
-  color: #333;
-  text-align: center;
-  line-height: 60px;
-}
 
-.el-aside {
-  background-color: #D3DCE6;
-  color: #333;
-  text-align: center;
-  line-height: 200px;
-}
-
-.el-main {
-  background-color: #E9EEF3;
-  color: #333;
-  text-align: center;
-  line-height: auto;
-}
-
-body>.el-container {
-  margin-bottom: 40px;
-}
-
-.el-container:nth-child(5) .el-aside,
-.el-container:nth-child(6) .el-aside {
-  line-height: 260px;
-}
-
-.el-container:nth-child(7) .el-aside {
-  line-height: 320px;
-}
 </style>
